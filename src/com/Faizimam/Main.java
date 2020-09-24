b53c33b17196afe0c1e8fa89225b00045838b22e
Faiz Imam <70627962+Faiziimam@users.noreply.github.com>
2020-09-24 11:29:26 +0530

    Update two additional option

--- src/com/Faizimam/Main.java
+++ src/com/Faizimam/Main.java
@@ -6,8 +6,21 @@ public class Main {
         Email obj=new Email("Faiz","Imam");
         System.out.println(obj.showInfo());
         System.out.println("============================================================================================");
-        obj.setPassword("Faiz Imam");
-        //System.out.println("Your Changed Password :"+obj.getPassword(length));
-    }
+        System.out.println("To change Password Enter--> a");
+        System.out.println("To Add Alternate Email Enter--> b");
 
+        char option = s.next().charAt(0);
+        if (option == 'a') {
+            System.out.println("Enter New Password :");
+            obj.setPassword(s.next());
+            System.out.println("Your New Password :"+obj.getPassword());
+        }
+        if(option=='b'){
+            System.out.println("Enter your Alternate Email :");
+            obj.setaltEmail(s.next());
+            System.out.println("Your Alternate Email :"+obj.getAlternateEmail());
+      
+       }
+
+    }
 }
